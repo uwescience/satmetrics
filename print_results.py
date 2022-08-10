3ceaef443ef87f9e766d58467f03e45135a49b8b
Abhilash Biswas <abhilasb@andrew.cmu.edu>
2022-08-10 15:00:21 -0400

    Work in progress

--- print_results.py
+++ print_results.py
@@ -6,34 +6,56 @@ def extract_yaml(filepath):
     
     return results
 
-def print_lines(dict, streak_properties_list):
-    str = ''
+def print_lines(properties_dict, streak_properties_list):
+    properties_string = ''
     for property in streak_properties_list:
-        str += dict[property] + '   '
+        properties_string += str(properties_dict[property]) + '   '
 
-    return str
+    return properties_string
 
-class PrintResults:
-    def __init__(self, filepath):
-        self.filepath = filepath
-        self.current_dict = None
+def print_table(results_dict, streak_properties_list):
+    print_list = []
+
+    for main_file in results_dict.keys():
+        string = main_file + '  '
+
+        for sub_file in results_dict[main_file].keys():
+            string += sub_file + '  '
+
+            for streak_id in results_dict[main_file][sub_file].keys():
+                string += str(streak_id) + '    '
+                string += print_lines(results_dict[main_file][sub_file][streak_id], streak_properties_list)
+        
+        print_list.append(string)
     
-    def print_result_lines(self, streak_properties_list):
-        print_lines = []
-        results_dict = extract_yaml(self.filepath)
-        self.current_dict = results_dict
+    return print_list
 
-        for main_file in results_dict.keys():
-            string = main_file + '  '
+def write_outfile(outfile_path, results_table, header):
+    with open (outfile_path, 'w') as f:
+        f.write("%s\n" % header)
+        for result_line in results_table:
+            f.write("%s\n" % result_line)
+    
 
-            self.current_dict = self.current_dict[main_file]
-            for sub_file in self.current_dict.keys():
-                string += sub_file + '  '
-                self.current_dict = self.current_dict[sub_file]
-                string += print_lines(self.current_dict, streak_properties_list)
+if __name__ == '__main__':
+    filepath = 'test_results.yaml'
+    streak_properties_list = ['amplitude','mean_brightness','sigma','fwhm']
 
-        return print_lines
-            
+    results_dict = extract_yaml(filepath)
+    header = ['main_file', 'sub_file', 'streak_id']
+    header.append(streak_properties_list)
+
+    final_results_list = print_table(results_dict, streak_properties_list)
+    outfile = 'results_table.txt'
+
+    write_outfile(outfile, final_results_list, header)
+
+    
+    
+
+
+
+
         
 
 

5b23cb8e43042e0fe8c2531aeda5e4533078139e
Abhilash Biswas <abhilasb@andrew.cmu.edu>
2022-08-11 14:04:57 -0400

    added convert to csv function

--- print_results.py
+++ print_results.py
@@ -1,4 +1,7 @@
 import yaml
+import csv
+import pandas as pd
+
 
 def extract_yaml(filepath):
     with open(filepath, 'r') as f:
@@ -7,38 +10,51 @@ def extract_yaml(filepath):
     return results
 
 def print_lines(properties_dict, streak_properties_list):
-    properties_string = ''
+    properties_list = []
     for property in streak_properties_list:
-        properties_string += str(round(properties_dict[property],2)) + '   '
+        properties_list.append(str(round(properties_dict[property],2)))
 
-    return properties_string
+    return properties_list
 
 def print_table(results_dict, streak_properties_list):
     print_list = []
 
     for main_file in results_dict.keys():
-        string = main_file + '  '
+        main_file_list = []
+        #string = main_file + '  '
+        main_file_list.append(main_file)
 
         for sub_file in results_dict[main_file].keys():
-            string += sub_file + '  '
+            #string += sub_file + '  '
+            main_file_list.append(sub_file)
 
             for streak_id in results_dict[main_file][sub_file].keys():
-                string += str(streak_id) + '    '
-                string += print_lines(results_dict[main_file][sub_file][streak_id], streak_properties_list)
+                #string += str(streak_id) + '    '
+                main_file_list.append(str(streak_id))
+                properties_list = print_lines(results_dict[main_file][sub_file][streak_id], streak_properties_list)
+
+                for property in properties_list:
+                    main_file_list.append(property)
         
-        print_list.append(string)
+        print_list.append(main_file_list)
     
     return print_list
 
 def write_outfile(outfile_path, results_table, header):
     with open (outfile_path, 'w') as f:
-        f.write("%s\n" % header)
+        writer = csv.writer(f)
+
+        writer.writerow(header)
         for result_line in results_table:
-            f.write("%s\n" % result_line)
-    
+            writer.writerow(result_line)
+
+def convert_to_csv(text_file, csv_outpath):
+    df = pd.read_csv(text_file,delimiter=',')
+    df.to_csv(csv_outpath)
+
 
 if __name__ == '__main__':
-    filepath = 'test_results.yaml'
+    filepath = 'results.yaml'
     streak_properties_list = ['amplitude','mean_brightness','sigma','fwhm']
 
     results_dict = extract_yaml(filepath)
@@ -46,15 +62,17 @@ if __name__ == '__main__':
     for property in streak_properties_list:
         header.append(property)
 
+    '''
     header_string = ''
     for h in header:
-        header_string += h + '      '
-    
+        header_string += h + ','
+    '''
+
     final_results_list = print_table(results_dict, streak_properties_list)
-    outfile = 'results_table.txt'
+    outfile = 'results_table.csv'
 
-    write_outfile(outfile, final_results_list, header_string)
-
+    write_outfile(outfile, final_results_list, header)
+    convert_to_csv(outfile, 'results_formatted.csv')
 
     
 
